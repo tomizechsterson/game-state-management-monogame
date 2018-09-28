12966e63820851268218f7dd5bffca41c85e69c7
Tomas <tomizechsterson@gmail.com>
2018-09-27 21:04:29 -0700

    Clean up OptionsMenuScreen
    Even bust out some string interpolation for fun

--- Screens/OptionsMenuScreen.cs
+++ Screens/OptionsMenuScreen.cs
@@ -5,130 +5,83 @@ namespace MgGSM.Screens
     // in various hopefully useful ways.
     public class OptionsMenuScreen : MenuScreen
     {
-        #region Fields
-
-        MenuEntry ungulateMenuEntry;
-        MenuEntry languageMenuEntry;
-        MenuEntry frobnicateMenuEntry;
-        MenuEntry elfMenuEntry;
-
-        enum Ungulate
+        private enum Ungulate
         {
             BactrianCamel,
             Dromedary,
             Llama,
         }
 
-        static Ungulate currentUngulate = Ungulate.Dromedary;
+        private readonly MenuEntry _ungulateMenuEntry;
+        private readonly MenuEntry _languageMenuEntry;
+        private readonly MenuEntry _frobnicateMenuEntry;
+        private readonly MenuEntry _elfMenuEntry;
 
-        static string[] languages = { "C#", "French", "Deoxyribonucleic acid" };
-        static int currentLanguage = 0;
+        private static Ungulate _currentUngulate = Ungulate.Dromedary;
+        private static readonly string[] Languages = { "C#", "French", "Deoxyribonucleic acid" };
+        private static int _currentLanguage;
+        private static bool _frobnicate = true;
+        private static int _elf = 23;
 
-        static bool frobnicate = true;
-
-        static int elf = 23;
-
-        #endregion
-
-        #region Initialization
-
-
-        /// <summary>
-        /// Constructor.
-        /// </summary>
-        public OptionsMenuScreen()
-            : base("Options")
+        public OptionsMenuScreen() : base("Options")
         {
-            // Create our menu entries.
-            ungulateMenuEntry = new MenuEntry(string.Empty);
-            languageMenuEntry = new MenuEntry(string.Empty);
-            frobnicateMenuEntry = new MenuEntry(string.Empty);
-            elfMenuEntry = new MenuEntry(string.Empty);
+            _ungulateMenuEntry = new MenuEntry(string.Empty);
+            _languageMenuEntry = new MenuEntry(string.Empty);
+            _frobnicateMenuEntry = new MenuEntry(string.Empty);
+            _elfMenuEntry = new MenuEntry(string.Empty);
 
             SetMenuEntryText();
 
-            MenuEntry back = new MenuEntry("Back");
+            var back = new MenuEntry("Back");
 
-            // Hook up menu event handlers.
-            ungulateMenuEntry.Selected += UngulateMenuEntrySelected;
-            languageMenuEntry.Selected += LanguageMenuEntrySelected;
-            frobnicateMenuEntry.Selected += FrobnicateMenuEntrySelected;
-            elfMenuEntry.Selected += ElfMenuEntrySelected;
+            _ungulateMenuEntry.Selected += UngulateMenuEntrySelected;
+            _languageMenuEntry.Selected += LanguageMenuEntrySelected;
+            _frobnicateMenuEntry.Selected += FrobnicateMenuEntrySelected;
+            _elfMenuEntry.Selected += ElfMenuEntrySelected;
             back.Selected += OnCancel;
-            
-            // Add entries to the menu.
-            MenuEntries.Add(ungulateMenuEntry);
-            MenuEntries.Add(languageMenuEntry);
-            MenuEntries.Add(frobnicateMenuEntry);
-            MenuEntries.Add(elfMenuEntry);
+
+            MenuEntries.Add(_ungulateMenuEntry);
+            MenuEntries.Add(_languageMenuEntry);
+            MenuEntries.Add(_frobnicateMenuEntry);
+            MenuEntries.Add(_elfMenuEntry);
             MenuEntries.Add(back);
         }
 
-
-        /// <summary>
-        /// Fills in the latest values for the options screen menu text.
-        /// </summary>
-        void SetMenuEntryText()
+        // Fills in the latest values for the options screen menu text.
+        private void SetMenuEntryText()
         {
-            ungulateMenuEntry.Text = "Preferred ungulate: " + currentUngulate;
-            languageMenuEntry.Text = "Language: " + languages[currentLanguage];
-            frobnicateMenuEntry.Text = "Frobnicate: " + (frobnicate ? "on" : "off");
-            elfMenuEntry.Text = "elf: " + elf;
+            _ungulateMenuEntry.Text = $"Preferred ungulate: {_currentUngulate}";
+            _languageMenuEntry.Text = $"Language: {Languages[_currentLanguage]}";
+            _frobnicateMenuEntry.Text = $"Frobnicate: {(_frobnicate ? "on" : "off")}";
+            _elfMenuEntry.Text = $"elf: {_elf.ToString()}";
         }
 
-
-        #endregion
-
-        #region Handle Input
-
-
-        /// <summary>
-        /// Event handler for when the Ungulate menu entry is selected.
-        /// </summary>
-        void UngulateMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        private void UngulateMenuEntrySelected(object sender, PlayerIndexEventArgs e)
         {
-            currentUngulate++;
+            _currentUngulate++;
 
-            if (currentUngulate > Ungulate.Llama)
-                currentUngulate = 0;
+            if (_currentUngulate > Ungulate.Llama)
+                _currentUngulate = 0;
 
             SetMenuEntryText();
         }
 
-
-        /// <summary>
-        /// Event handler for when the Language menu entry is selected.
-        /// </summary>
-        void LanguageMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        private void LanguageMenuEntrySelected(object sender, PlayerIndexEventArgs e)
         {
-            currentLanguage = (currentLanguage + 1) % languages.Length;
-
+            _currentLanguage = (_currentLanguage + 1) % Languages.Length;
             SetMenuEntryText();
         }
 
-
-        /// <summary>
-        /// Event handler for when the Frobnicate menu entry is selected.
-        /// </summary>
-        void FrobnicateMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        private void FrobnicateMenuEntrySelected(object sender, PlayerIndexEventArgs e)
         {
-            frobnicate = !frobnicate;
-
+            _frobnicate = !_frobnicate;
             SetMenuEntryText();
         }
 
-
-        /// <summary>
-        /// Event handler for when the Elf menu entry is selected.
-        /// </summary>
-        void ElfMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        private void ElfMenuEntrySelected(object sender, PlayerIndexEventArgs e)
         {
-            elf++;
-
+            _elf++;
             SetMenuEntryText();
         }
-
-
-        #endregion
     }
 }
