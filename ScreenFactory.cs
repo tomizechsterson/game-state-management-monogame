544847e9a3c5d514d535b4e4475b198d9d89b234
Tomas <tomizechsterson@gmail.com>
2018-09-27 22:23:09 -0700

    Small touch-up for ScreenFactory

--- ScreenFactory.cs
+++ ScreenFactory.cs
@@ -3,6 +3,8 @@ using MgGSM.StateManagement;
 
 namespace MgGSM
 {
+    // Our game's implementation of IScreenFactory which can handle creating the screens
+    // when resuming from being tombstoned.
     public class ScreenFactory : IScreenFactory
     {
         public GameScreen CreateScreen(Type screenType)
